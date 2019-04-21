8d58ed657eabbf715ae67135db727494b24da275
Sho Nakatani <lay.sakura@gmail.com>
2019-04-21 10:57:40 +0900

    auto push

--- ci/before_script.sh
+++ ci/before_script.sh
@@ -3,3 +3,28 @@ set -eux
 
 rustup component add rustfmt
 cargo readme || cargo install cargo-readme  # skip if already available
+
+## Auto commit & push by CI
+git clone git@github.com:laysakura/fid-rs.git
+(
+    cd fid-rs
+    git checkout ${TRAVIS_PULL_REQUEST_BRANCH}
+
+    committed=0
+
+    ### README.md from src/lib.rs
+    cargo readme > README.md
+    git add README.md
+    git commit -m 'cargo readme > README.md' && committed=1
+
+    ### cargo fmt
+    cargo fmt --all
+    git add -A
+    git commit -m 'cargo fmt --all' && committed=1
+
+    ### git push
+    git push origin ${TRAVIS_PULL_REQUEST_BRANCH}
+
+    ### Stop build if anything updated in remote
+    [ $committed -eq 1 ] && exit 1
+)
--- ci/script.sh
+++ ci/script.sh
@@ -1,19 +1,6 @@
 #!/usr/bin/env bash
 set -eux
 
-## Auto commit & push by CI
-### README.md from src/lib.rs
-cargo readme > README.md
-git add README.md
-git commit -m 'cargo readme > README.md' &&:
-### cargo fmt
-cargo fmt --all
-git add -A
-git commit -m 'cargo fmt --all' &&:
-### git push
-git fetch
-git push https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git ${TRAVIS_PULL_REQUEST_BRANCH}
-
 ## cargo build ~ bench
 cargo build --release --verbose --all
 cargo test --release --verbose --all
