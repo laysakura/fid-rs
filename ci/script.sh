3467d50518e06ff8d43d1f10791fdf634493cffe
Sho Nakatani <lay.sakura@gmail.com>
2019-04-21 19:45:49 +0900

    wip

--- ci/script.sh
+++ ci/script.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 set -eux
 
-## cargo build ~ bench
-cargo build --release --verbose --all
-cargo test --release --verbose --all
-cargo doc
-cargo bench --all
-
-## Move criterion's HTML report into doc/ dir in order to be uploaded in github.io
-rm -rf target/doc/criterion && mv target/criterion target/doc/
+# cargo build --release --verbose --all
+# cargo test --release --verbose --all
